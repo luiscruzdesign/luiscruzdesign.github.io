1b6ef04a2321e2cb9ab0dc4a4e0a71ca4b430091
Teste modificação de cores layout <luiscruz@Luiss-Mac-mini.local>
2017-07-20 23:58:03 -0300

    Funcionalidade etapas no footer

--- public_html/calculadora.js
+++ public_html/calculadora.js
@@ -12,18 +12,26 @@ $( document ).ready(function() {
   $("#confirma-cidade-seletor, #confirma-cidade").click(function() {
     $("#localizacao").hide();
     $("#consumo-medio").show();
+    $("#etapa-localizacao").removeClass("active");
+    $("#etapa-consumo").addClass("active");
   });
   $("#confirma-consumo").click(function() {
     $("#consumo-medio").hide();
     $("#face-da-casa").show();
+    $("#etapa-consumo").removeClass("active");
+    $("#etapa-face").addClass("active");
   });
   $("#face-norte, #face-sul").click(function() {
     $("#face-da-casa").hide();
     $("#contato").show();
+    $("#etapa-face").removeClass("active");
+    $("#etapa-contato").addClass("active");
   });
   $("#confirma-contato").click(function() {
     $("#contato").hide();
     $("#kit-energia-solar").show();
+    $("#etapa-contato").removeClass("active");
+    $("#etapa-resultado").addClass("active");
   });
   $("#confirma-logger, #rejeita-logger").click(function() {
     $("#kit-energia-solar").hide();
